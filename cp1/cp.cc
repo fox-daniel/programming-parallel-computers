77ac4a0ad76f7866fddd21b10c1b83263b098b0a
Daniel Fox <foxdanie@gmail.com>
2025-10-01 11:42:16 -0400

    improve algo by storing normalized rows

--- cp1/cp.cc
+++ cp1/cp.cc
@@ -2,7 +2,7 @@
 #include <cmath>
 
 
-void normalize_row(int a, int nx, const float *data, std::vector<double>& row) {
+void normalize_rows(int a, int nx, const float *data, std::vector<double>& normalized) {
     double mean = 0;
     double len = 0;
     for (int k=0; k<nx; k++) {
@@ -10,30 +10,24 @@ void normalize_row(int a, int nx, const float *data, std::vector<double>& row) {
     }
     mean = mean / nx;
     for (int k=0; k<nx; k++) {
-        row[k] = data[k+ a*nx] - mean;
+        normalized[k+ a*nx] = data[k + a*nx] - mean;
    }
     for (int k=0; k<nx; k++) {
-        len += row[k]*row[k];
+        len += normalized[k + a*nx]*normalized[k + a*nx];
     }
     len = sqrt(len);
     // std::cout << "stdev:" << std::endl;
     // std::cout << len << std::endl;
     for (int k=0; k<nx; k++) {
-        row[k] = row[k]/len;
+        normalized[k + a*nx] = normalized[k + a*nx]/len;
     }
 }
 
 
-float correlate_rows(int a, int b, int nx, const float *data) {
+float correlate_rows(int a, int b, int nx, std::vector<double>& normalized) {
     double correlation = 0;
-    std::vector<double> row1(nx);
-    std::vector<double> row2(nx);
-    normalize_row(a, nx, data, row1);
-    normalize_row(b, nx, data, row2);
-    // print_vector("row1", row1);
-    // print_vector("row2", row2);
     for (int i=0; i<nx; i++) {
-        correlation += row1[i]*row2[i];
+        correlation += normalized[i+nx*a]*normalized[i+nx*b];
     }
     return correlation;
 }
@@ -47,9 +41,13 @@ This is the function you need to implement. Quick reference:
 - only parts with 0 <= j <= i < ny need to be filled
 */
 void correlate(int ny, int nx, const float *data, float *result) {
+    std::vector<double> normalized(ny*nx);
+    for (int i=0; i<ny; i++) {
+        normalize_rows(i, nx, data, normalized);
+    }
     for (int i=0; i<ny; i++) {
         for (int j=0; j<=i; j++) {
-            result[i+j*ny] = correlate_rows(i, j, nx, data);
+            result[i+j*ny] = correlate_rows(i, j, nx, normalized);
         }
     }
 }
