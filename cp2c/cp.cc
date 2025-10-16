8f0225e57b444692ab60118f8805746f8f5b3636
Daniel Fox <foxdanie@gmail.com>
2025-10-16 14:33:31 -0400

    compiles with double4_t ops but is off by small amount

--- cp2c/cp.cc
+++ cp2c/cp.cc
@@ -2,6 +2,33 @@
 #include <cmath>
 
 
+typedef float float8_t __attribute__ ((vector_size (8 * sizeof(float))));
+typedef double double4_t __attribute__ ((vector_size (4 * sizeof(double))));
+
+double4_t float4_to_double4(float x1, float x2, float x3, float x4) {
+    return (double4_t){(double)x1, (double)x2, (double)x3, (double)x4};
+}
+
+void pad(int ny, int nx, int nxp, std::vector<double>& normalized, std::vector<double>& pdata) {
+    for (int i=0; i<ny; i+=1) {
+        for (int j=0; j<nx; j += 1) {
+            pdata[j + i*nxp] = normalized[j + i*nx];
+        }
+    }
+
+}
+
+void convert_dtype(int ny, int nx, int m, std::vector<double>& pdata, std::vector<double4_t>& vdata) {
+    // pdata is required to represent an ny x nx matrix where nx (column length) is divisible by 4
+
+    for (int i=0; i<ny; i+=1) {
+        for (int j=0; j<nx; j += 4) {
+            vdata[j/4 + m*i] = float4_to_double4(pdata[j + nx*i], pdata[1 + j + nx*i], pdata[2 + j + nx*i], pdata[3 + j + nx*i]);
+        }
+    }
+}
+
+
 void normalize_rows(int a, int nx, const float *data, std::vector<double>& normalized) {
     double mean = 0;
     double len = 0;
@@ -26,12 +53,12 @@ void normalize_rows(int a, int nx, const float *data, std::vector<double>& norma
 }
 
 
-float correlate_rows(int a, int b, int nx, std::vector<double>& normalized) {
-    double correlation = 0;
-    for (int i=0; i<nx; i++) {
-        correlation += normalized[i+nx*a]*normalized[i+nx*b];
+float correlate_rows(int a, int b, int m, std::vector<double4_t>& vdata) {
+    double4_t correlation = {0.0, 0.0, 0.0, 0.0};
+    for (int i=0; i<m; i++) {
+        correlation += vdata[i+m*a]*vdata[i+m*b];
     }
-    return correlation;
+    return correlation[0]+correlation[1]+correlation[2]+correlation[3];
 }
 
 /*
@@ -47,9 +74,15 @@ void correlate(int ny, int nx, const float *data, float *result) {
     for (int i=0; i<ny; i++) {
         normalize_rows(i, nx, data, normalized);
     }
+    const int m = ceil(nx / 4.0);
+    const int nxp = m * 4;
+    std::vector<double> pdata(ny*nxp);
+    pad(ny, nx, nxp, normalized, pdata);
+    std::vector<double4_t> vdata(ny*m);
+    convert_dtype(ny, nxp, m, pdata, vdata);
     for (int i=0; i<ny; i++) {
         for (int j=0; j<=i; j++) {
-            result[i+j*ny] = correlate_rows(i, j, nx, normalized);
+            result[i+j*ny] = correlate_rows(i, j, m, vdata);
         }
     }
 }
